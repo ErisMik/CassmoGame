3170ce872a4588112f7e60602532cc19170a673f
PeterFajner <fajner1+Github@gmail.com>
2014-11-01 01:58:48 -0600

    Can switch from one gravity source to another by touching them with the wheel

--- Walking Test/Assets/Scripts/Gravity.js
+++ Walking Test/Assets/Scripts/Gravity.js
@@ -2,22 +2,23 @@
 /* This script implements variable gravity direction. Billy's Wheel has a Constant Force which either points towards
 towards the floor of the ship he is on. If no contact with an antigrav floor is detected for 10 frames, he is accelerated
 downwards.
+Currently, planet is default.
 */
 
 private var recentCollision:int = 10;
-private var Floor:GameObject;
-var ground:GameObject;
+var floor:GameObject; // current ground
+var ground:GameObject; // planet
 var forceAmount:int; //positive
 
 function OnCollisionStay (collision:Collision){
 	if (collision.collider.tag == "AntiGravFloor") {
-		Floor = collision.gameObject;
-		recentCollision = 10;
+		floor = collision.gameObject;
+		recentCollision = 100;
 	};
 }
 
 function Start () {
-	Floor = ground;
+	floor = ground;
 }
 
 function Update () {
@@ -25,13 +26,10 @@ function Update () {
 		recentCollision -= 1;
 	}
 	else if (recentCollision <= 0) {
-		Floor = ground;
+		floor = ground;
 	};
 
-	var xDiff = (Floor.transform.position.x - transform.position.x);
-	var zDiff = (Floor.transform.position.z - transform.position.z);
-	var Force = (Floor.transform.position - transform.position).normalized * forceAmount * Time.smoothDeltaTime;
-	rigidbody.AddForce(Force);
-	//Debug.Log(Force);
+	var force = (floor.transform.position - transform.position).normalized * forceAmount * Time.smoothDeltaTime;
+	rigidbody.AddForce(force);
 
 }
--- Walking Test/Assets/Scripts/TorsoStability.js
+++ Walking Test/Assets/Scripts/TorsoStability.js
@@ -1,16 +1,20 @@
 ﻿#pragma strict
 
-var Ground:GameObject;
-var Torque:float;
+private var floor:GameObject;
+var torque:float;
+var wheel:GameObject;
 
 function Start () {
+}
 
+function Update () {
+	floor = wheel.GetComponent(Gravity).floor;
 }
 
 function FixedUpdate () {
 
 		//bummed from http://wiki.unity3d.com/index.php?title=TorqueLookRotation
-		var targetDelta:Vector3 = Ground.transform.position - transform.position;
+		var targetDelta:Vector3 = floor.transform.position - transform.position;
  
 		//get the angle between transform.forward and target delta
 		var angleDiff:float = Vector3.Angle(-transform.up, targetDelta);
@@ -20,5 +24,5 @@ function FixedUpdate () {
 		var cross:Vector3 = Vector3.Cross(-transform.up, targetDelta);
  
 		// apply torque along that axis according to the magnitude of the angle.
-		rigidbody.AddTorque(cross * angleDiff * Torque);
+		rigidbody.AddTorque(cross * angleDiff * torque);
 }
