83d3c461d1b1702a2b6a2e1e40deb5787f9dccdb
PeterFajner <fajner1+Github@gmail.com>
2014-11-01 00:57:45 -0600

    Made ground round

---
+++ Walking Test/Assets/Scripts/Gravity.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+/* This script implements variable gravity direction. Billy's Wheel has a Constant Force which either points towards
+towards the floor of the ship he is on. If no contact with an antigrav floor is detected for 10 frames, he is accelerated
+downwards.
+*/
+
+private var recentCollision:int = 10;
+private var Floor:GameObject;
+var ground:GameObject;
+var forceAmount:int; //positive
+
+function OnCollisionStay (collision:Collision){
+	if (collision.collider.tag == "AntiGravFloor") {
+		Floor = collision.gameObject;
+		recentCollision = 10;
+	};
+}
+
+function Start () {
+	Floor = ground;
+}
+
+function Update () {
+	if (recentCollision > 0) {
+		recentCollision -= 1;
+	}
+	else if (recentCollision <= 0) {
+		Floor = ground;
+	};
+
+	var xDiff = (Floor.transform.position.x - transform.position.x);
+	var zDiff = (Floor.transform.position.z - transform.position.z);
+	var Force = (Floor.transform.position - transform.position).normalized * forceAmount * Time.smoothDeltaTime;
+	rigidbody.AddForce(Force);
+	//Debug.Log(Force);
+
+}
---
+++ Walking Test/Assets/Scripts/TorsoStability.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var Ground:GameObject;
+var Torque:float;
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+
+		//bummed from http://wiki.unity3d.com/index.php?title=TorqueLookRotation
+		var targetDelta:Vector3 = Ground.transform.position - transform.position;
+ 
+		//get the angle between transform.forward and target delta
+		var angleDiff:float = Vector3.Angle(-transform.up, targetDelta);
+ 
+		// get its cross product, which is the axis of rotation to
+		// get from one vector to the other
+		var cross:Vector3 = Vector3.Cross(-transform.up, targetDelta);
+ 
+		// apply torque along that axis according to the magnitude of the angle.
+		rigidbody.AddTorque(cross * angleDiff * Torque);
+}
