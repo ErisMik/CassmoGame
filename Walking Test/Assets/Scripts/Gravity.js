290f2440b4bb1435042170ef52c5a9b42995a7e2
Eric Mikulin <ericm99@gmail.com>
2015-01-17 18:28:59 -0700

    added GPL
    Hopefully I didn't miss any scripts and I actually added this right
    :page_facing_up:

--- Walking Test/Assets/Scripts/Gravity.js
+++ Walking Test/Assets/Scripts/Gravity.js
@@ -1,4 +1,23 @@
-﻿#pragma strict
+﻿ /*   
+ 	This file is part of Cassmo
+
+    Copyright (C) 2014-2015  Peter Fajner & Eric Mikulin
+
+    Cassmo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cassmo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma strict
 /* This script implements variable gravity direction. Billy's Wheel has a Constant Force which either points towards
 towards the floor of the ship he is on. If no contact with an antigrav floor is detected for 10 frames, he is accelerated
 downwards.
