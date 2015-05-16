427fcda7ad7135f8bb7e01fab661dae15ecf5f4a
Eric Mikulin <ericm99@gmail.com>
2015-05-16 00:10:56 -0600

    Added moving player model

---
+++ TestingGrounds/Assets/Scripts/playCantrol.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playCantrol : MonoBehaviour {
+	public float MoveSpeed = 10; 
+	public float RotateSpeed = 40;
+		
+	void Start() { } // Update is called once per frame
+		
+	void Update () { 
+		// Amount to Move 
+		float MoveForward = Input.GetAxis("Vertical")*MoveSpeed*Time.deltaTime;
+		float MoveRotate = Input.GetAxis("Horizontal")*RotateSpeed*Time.deltaTime;
+			
+		// Move the player
+		transform.Translate(Vector3.forward * MoveForward);
+		transform.Rotate(Vector3.up * MoveRotate);
+			
+	}
+}
