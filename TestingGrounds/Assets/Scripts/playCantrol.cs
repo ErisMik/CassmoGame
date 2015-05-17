bb47af30bfa545b6ce1cabd3bbd8cb4497f57a8a
Eric Mikulin <ericm99@gmail.com>
2015-05-17 14:44:32 -0600

    FajnerCube cannot into space
    I fixed the bug where the FajnerCube could jump again in the air,
    seeming as if he could fly. This is no longer. I solved it be adding
    another condition to the jump if statement. This condition is a
    raytrace downwards until it hits an object, and it must be within the
    height (+0.1).

--- TestingGrounds/Assets/Scripts/playCantrol.cs
+++ TestingGrounds/Assets/Scripts/playCantrol.cs
@@ -7,11 +7,13 @@ public class playCantrol : MonoBehaviour {
 	public float JumpForce = 10;
 
 	public Rigidbody rb;
+	public float distToGround;
 
 	bool jump;
 		
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		distToGround = GetComponent<Collider>().bounds.extents.y + 0.1f;
 	}
 		
 	void Update () {
@@ -25,6 +27,9 @@ public class playCantrol : MonoBehaviour {
 			Cursor.visible = false;
 		}
 
+		//Check to see if character is touching the ground
+		bool grounded = Physics.Raycast(transform.position, -Vector3.up, distToGround);
+
 		// Amount to Move
 		float MoveForward = Input.GetAxis("Vertical")*MoveSpeed*Time.deltaTime;
 		float MoveLeft = Input.GetAxis("Horizontal")*MoveSpeed*Time.deltaTime;
@@ -36,7 +41,7 @@ public class playCantrol : MonoBehaviour {
 		transform.Rotate(Vector3.up * MoveRotate);
 
 		//Check for jump
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
 			jump = true;
 		}
 	}
