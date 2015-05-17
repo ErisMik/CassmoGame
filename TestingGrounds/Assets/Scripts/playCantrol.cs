3985ab753d4ad995ca993fef8c7b80166ca10321
Eric Mikulin <ericm99@gmail.com>
2015-05-16 19:16:13 -0600

    TestCube Working good
    Added all the nessicary things
    
    - Strafe Movement
    - Jump
    - Mouse look (Horizontal Only)
    - Cursor locks and disappears when in focus, can be unlocked and
    visible by pressing escape

--- TestingGrounds/Assets/Scripts/playCantrol.cs
+++ TestingGrounds/Assets/Scripts/playCantrol.cs
@@ -4,17 +4,48 @@ using System.Collections;
 public class playCantrol : MonoBehaviour {
 	public float MoveSpeed = 10; 
 	public float RotateSpeed = 40;
+	public float JumpForce = 10;
+
+	public Rigidbody rb;
+
+	bool jump;
 		
-	void Start() { } // Update is called once per frame
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
 		
-	void Update () { 
-		// Amount to Move 
+	void Update () {
+		//Lock the cursor
+		if(Input.GetKey(KeyCode.Escape)){
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		else{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		// Amount to Move
 		float MoveForward = Input.GetAxis("Vertical")*MoveSpeed*Time.deltaTime;
-		float MoveRotate = Input.GetAxis("Horizontal")*RotateSpeed*Time.deltaTime;
-			
+		float MoveLeft = Input.GetAxis("Horizontal")*MoveSpeed*Time.deltaTime;
+		float MoveRotate = Input.GetAxis ("Mouse X") * RotateSpeed * Time.deltaTime;
+
 		// Move the player
-		transform.Translate(Vector3.forward * MoveForward);
+		Vector3 MoveAmnt = new Vector3(MoveLeft, 0, MoveForward);
+		transform.Translate(MoveAmnt);
 		transform.Rotate(Vector3.up * MoveRotate);
-			
+
+		//Check for jump
+		if(Input.GetKeyDown(KeyCode.Space)){
+			jump = true;
+		}
+	}
+
+	void FixedUpdate (){
+		//Jump the player
+		if (jump) {
+			rb.AddForce(new Vector3(0, 1, 0) * JumpForce);
+			jump = false;
+		}
 	}
 }
